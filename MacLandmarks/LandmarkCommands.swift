392acf20bac2127a502d77f7418f6c51436704a5
ochi <mune.ochi@gmail.com>
2022-05-08 14:54:20 +0900

    tutorial10 section5

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by 越智宗洋 on 2022/05/08.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
